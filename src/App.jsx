37dc36b0f463e4071abeefb468294adf145ec8e2
Erik Lima <eriklima832@gmail.com>
2024-11-24 14:33:14 -0300

    fix: resolvendo bug do arquivo forma no host

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
 import Resume from "./components/Resume";
-import Form from "./components/Form";
+import Form from "./components/form/index";
 
 const App = () => {
   const data = localStorage.getItem("transactions");
