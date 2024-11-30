143cdda3e342bde8c16a46234867637e4d4f0703
Erik Lima <eriklima832@gmail.com>
2024-11-30 10:45:17 -0300

    style: mudando cor nos componentes Header e Button para #6610f2

--- src/components/Header/styles.jsx
+++ src/components/Header/styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   height: 150px;
   text-align: center;
-  background: teal;
+  background: #6610f2;
 `;
 
 export const Header = styled.h1``;
